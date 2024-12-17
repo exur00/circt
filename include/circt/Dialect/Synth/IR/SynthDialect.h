0f389c6833c5ff334cd4a1e55f785a865c91aded
exur00 <git@exuro.be>
2024-12-17 14:51:51 +0100

    add initial headers and tablegen files

---
+++ include/circt/Dialect/Synth/IR/SynthDialect.h
@@ -0,0 +1,23 @@
+
+//===- SynthDialect.h - Synth dialect declaration -------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the Synth MLIR dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_SYNTH_IR_SYNTHDIALECT_H
+#define CIRCT_DIALECT_SYNTH_IR_SYNTHDIALECT_H
+
+#include "circt/Support/LLVM.h"
+#include "mlir/IR/Dialect.h"
+
+// Pull in the Dialect definition.
+#include "circt/Dialect/Synth/IR/SynthDialect.h.inc"
+
+#endif // CIRCT_DIALECT_SYNTH_IR_SYNTHDIALECT_H
