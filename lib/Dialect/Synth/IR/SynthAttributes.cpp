8cc8edadb0326f3f5ce42db8cb5a55c50cb8c6d4
exur00 <git@exuro.be>
2025-01-04 11:04:52 +0100

    represent an enum of register information in synth dialect

---
+++ lib/Dialect/Synth/IR/SynthAttributes.cpp
@@ -0,0 +1,30 @@
+//===- SynthAttributes.cpp - Implement Synth attributes -----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/Synth/IR/SynthAttributes.h"
+#include "circt/Dialect/Synth/IR/SynthDialect.h"
+#include "circt/Dialect/Synth/IR/SynthTypes.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+using namespace circt;
+using namespace synth;
+
+#include "circt/Dialect/Synth/IR/SynthEnums.cpp.inc"
+#define GET_ATTRDEF_CLASSES
+#include "circt/Dialect/Synth/IR/SynthAttributes.cpp.inc"
+
+Type SynthEnumConstAttr::getType() { return nullptr; } // TODO: implement this. (see implementation in seq?)
+
+void SynthDialect::registerAttributes() {
+  addAttributes<
+#define GET_ATTRDEF_LIST
+#include "circt/Dialect/Synth/IR/SynthAttributes.cpp.inc"
+      >();
+}
--- lib/Dialect/Synth/IR/SynthDialect.cpp
+++ lib/Dialect/Synth/IR/SynthDialect.cpp
@@ -27,11 +27,12 @@ using namespace synth;
 void SynthDialect::initialize() {
   registerTypes();
   // Register operations.
-  //registerAttributes(); TODO: probably this also needed?
+  registerAttributes(); //TODO: probably this also needed?
   addOperations<
 #define GET_OP_LIST
 #include "circt/Dialect/Synth/IR/Synth.cpp.inc"
       >();
 }
 
+#define GET_ATTRDEF_CLASSES
 #include "circt/Dialect/Synth/IR/SynthDialect.cpp.inc"
